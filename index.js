face16eb86417cd5b0520bea62894cdc575dd224
Jayden Miller <124369541+Warriorofthelight@users.noreply.github.com>
2023-08-23 00:48:32 -0400

    Finished Css

---
+++ index.js
@@ -0,0 +1,28 @@
+// 1.The user choses which weapon tehy want
+
+// BTNS
+let rockb = document.getElementById("rockb");
+
+let paperb = document.getElementById("paperb");
+
+let scissorsb = document.getElementById("scissorsb");
+
+let start = document.getElementById("start");
+
+// images
+
+let rock = document.getElementById("rock");
+
+let paper = document.getElementById("paper");
+
+let scissors = document.getElementById("scissors");
+
+let rockO = document.getElementById("rockO");
+
+let paperO = document.getElementById("paperO");
+
+let scissorsO = document.getElementById("scissorsO");
+
+// 2.They start and the opponent shuffles through 1-3 and choses a random number
+
+// 3.A message stating wheter they lost or won is shown

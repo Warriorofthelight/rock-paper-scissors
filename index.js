633227d7396a789ed33a23c1c2e98028fa58fb7f
Jayden Miller <124369541+Warriorofthelight@users.noreply.github.com>
2023-08-23 01:24:12 -0400

    First JS commit

--- index.js
+++ index.js
@@ -23,6 +23,23 @@ let paperO = document.getElementById("paperO");
 
 let scissorsO = document.getElementById("scissorsO");
 
+// Choose weapon
+
+rockb.addEventListener("click", function () {
+  let rock = document.getElementById("rock");
+  rock.style.zIndex = "1";
+});
+
+paperb.addEventListener("click", function () {
+  let paper = document.getElementById("paper");
+  paper.style.zIndex = "1";
+});
+
+scissorsb.addEventListener("click", function () {
+  let scissors = document.getElementById("scissors");
+  scissors.style.zIndex = "1";
+});
+
 // 2.They start and the opponent shuffles through 1-3 and choses a random number
 
 // 3.A message stating wheter they lost or won is shown
